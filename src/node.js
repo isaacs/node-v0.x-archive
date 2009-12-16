fbaa25a8fc06ef8145ff3e63acce498c5c53cd17
isaacs <i@foohack.com>
2009-12-16 14:15:24 -0800

    Inadvertently introduced a bug in how single-dot path parts are handled.

--- src/node.js
+++ src/node.js
@@ -670,7 +670,7 @@ var posix = posixModule.exports;
 var pathModule = createInternalModule("path", function (exports) {
   exports.join = function () {
     var joined = "", 
-      dotre = /^\.\//g,
+      dotre = /^\.\//,
       dotreplace = "",
       dotdotre = /(^|(\/)([^\/]+\/)?)\.\.\//g,
       dotdotreplace = ""
@@ -679,7 +679,7 @@ var pathModule = createInternalModule("path", function (exports) {
 
       /* Some logic to shorten paths */
       if (part === ".") continue;
-      while (dotre.exec(part)) part.replace(dotre, dotreplace);
+      while (dotre.exec(part)) part = part.replace(dotre, dotreplace);
 
       if (i === 0) {
         part = part.replace(/\/*$/, "/");
