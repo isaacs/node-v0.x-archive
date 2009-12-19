nodejs/node

commit bfa36136dac787793bc5106da57cccbb8db1ce14

require() should throw error if module does.
Reported by Kris Zyp
http://groups.google.com/group/nodejs/browse_thread/thread/1feab0309bd5402b

src/node.js
@@ -892,7 +892,12 @@ Module.prototype.loadScript = function (filename, loadPromise) {
                 + "\n}; __wrap__;";
     var compiledWrapper = process.compile(wrapper, filename);
 
-    compiledWrapper.apply(self.exports, [self.exports, require, self, filename]);
+    try {
+      compiledWrapper.apply(self.exports, [self.exports, require, self, filename]);
+    } catch (e) {
+      loadPromise.emitError(e);
+      return;
+    }
 
     self.waitChildrenLoad(function () {
       self.loaded = true;

test/mjsunit/fixtures/throws_error.js
@@ -1,0 +1,1 @@
+throw new Error("blah");

test/mjsunit/test-module-loading.js
@@ -33,6 +33,14 @@ assert.equal("D", d3.D());
 assert.equal(true, d4.D instanceof Function);
 assert.equal("D", d4.D());
 
+var errorThrown = false;
+try {
+  require("./fixtures/throws_error");
+} catch (e) {
+  errorThrown = true;
+  assert.equal("blah", e.message);
+}
+
 process.addListener("exit", function () {
   assert.equal(true, a.A instanceof Function);
   assert.equal("A done", a.A());
@@ -48,6 +56,8 @@ process.addListener("exit", function () {
 
   assert.equal(true, d2.D instanceof Function);
   assert.equal("D done", d2.D());
+
+  assert.equal(true, errorThrown);
 
   puts("exit");
 });

isaacs/node-v0.x-archive

commit d88c6f2da56f3938389037981de058912a174718

Rethrow exceptions that make it to the main module
bfa36136dac787793bc5106da57cccbb8db1ce14 broke the exception system by
completely swallowing any exceptions. This patch "repairs" it by
rethrowing exceptions that bubble up to the main module. However, this
changes the stack trace output from:

----
node.js:22
  throw new Error("p() has moved. Use require('sys') to bring it back.");
        ^
Error: p() has moved. Use require('sys') to bring it back.
----

to:

----
node.js:962
  throw e;
  ^
Error: p() has moved. Use require('sys') to bring it back.
----

src/node.js
@@ -958,6 +958,10 @@ process.mainModule = createModule(".");
 var loadPromise = new process.Promise();
 process.mainModule.load(process.ARGV[1], loadPromise);
 
+loadPromise.addErrback(function(e) {
+  throw e;
+});
+
 // All our arguments are loaded. We've evaluated all of the scripts. We
 // might even have created TCP servers. Now we enter the main eventloop. If
 // there are no watchers on the loop (except for the ones that were