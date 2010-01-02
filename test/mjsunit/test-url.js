2762644154c28b47e444bb67488f5b2ca6032a72
isaacs <i@foohack.com>
2010-01-02 12:52:36 -0800

    Add a "query" member, which is just like search, but without the ? (the better to parse query string params with.)

--- test/mjsunit/test-url.js
+++ test/mjsunit/test-url.js
@@ -12,6 +12,7 @@ var parseTests = {
     "host": "www.narwhaljs.org",
     "hostname": "www.narwhaljs.org",
     "search": "?id=news",
+    "query": "id=news",
     "pathname": "/blog/categories"
   },
   "http://mt0.google.com/vt/lyrs=m@114&hl=en&src=api&x=2&y=2&z=3&s=" : {
@@ -27,6 +28,7 @@ var parseTests = {
     "host": "mt0.google.com",
     "hostname": "mt0.google.com",
     "search": "???&hl=en&src=api&x=2&y=2&z=3&s=",
+    "query": "??&hl=en&src=api&x=2&y=2&z=3&s=",
     "pathname": "/vt/lyrs=m@114"
   },
   "http://user:pass@mt0.google.com/vt/lyrs=m@114???&hl=en&src=api&x=2&y=2&z=3&s=" : {
@@ -36,6 +38,7 @@ var parseTests = {
     "auth": "user:pass",
     "hostname": "mt0.google.com",
     "search": "???&hl=en&src=api&x=2&y=2&z=3&s=",
+    "query": "??&hl=en&src=api&x=2&y=2&z=3&s=",
     "pathname": "/vt/lyrs=m@114"
   },
   "file:///etc/passwd" : {
@@ -66,6 +69,7 @@ var parseTests = {
    "hostname": "example.com",
    "hash": "#frag",
    "search": "?baz=quux",
+   "query": "baz=quux",
    "pathname": "/foo/bar"
   },
   "//user:pass@example.com:8000/foo/bar?baz=quux#frag" : {
@@ -76,6 +80,7 @@ var parseTests = {
    "hostname": "example.com",
    "hash": "#frag",
    "search": "?baz=quux",
+   "query": "baz=quux",
    "pathname": "/foo/bar"
   },
   "http://example.com?foo=bar#frag" : {
@@ -84,7 +89,8 @@ var parseTests = {
    "host": "example.com",
    "hostname": "example.com",
    "hash": "#frag",
-   "search": "?foo=bar"
+   "search": "?foo=bar",
+   "query": "foo=bar"
   },
   "http://example.com?foo=@bar#frag" : {
    "href": "http://example.com?foo=@bar#frag",
@@ -92,7 +98,8 @@ var parseTests = {
    "host": "example.com",
    "hostname": "example.com",
    "hash": "#frag",
-   "search": "?foo=@bar"
+   "search": "?foo=@bar",
+   "query": "foo=@bar"
   },
   "http://example.com?foo=/bar/#frag" : {
    "href": "http://example.com?foo=/bar/#frag",
@@ -100,7 +107,8 @@ var parseTests = {
    "host": "example.com",
    "hostname": "example.com",
    "hash": "#frag",
-   "search": "?foo=/bar/"
+   "search": "?foo=/bar/",
+   "query": "foo=/bar/"
   },
   "http://example.com?foo=?bar/#frag" : {
    "href": "http://example.com?foo=?bar/#frag",
@@ -108,7 +116,8 @@ var parseTests = {
    "host": "example.com",
    "hostname": "example.com",
    "hash": "#frag",
-   "search": "?foo=?bar/"
+   "search": "?foo=?bar/",
+   "query": "foo=?bar/"
   },
   "http://example.com#frag=?bar/#frag" : {
    "href": "http://example.com#frag=?bar/#frag",
@@ -121,6 +130,7 @@ var parseTests = {
    "href": "/foo/bar?baz=quux#frag",
    "hash": "#frag",
    "search": "?baz=quux",
+   "query": "baz=quux",
    "pathname": "/foo/bar"
   },
   "http:/foo/bar?baz=quux#frag" : {
@@ -128,13 +138,15 @@ var parseTests = {
    "protocol": "http:",
    "hash": "#frag",
    "search": "?baz=quux",
+   "query": "baz=quux",
    "pathname": "/foo/bar"
   },
   "mailto:foo@bar.com?subject=hello" : {
    "href": "mailto:foo@bar.com?subject=hello",
    "protocol": "mailto:",
    "pathname": "foo@bar.com",
-   "search": "?subject=hello"
+   "search": "?subject=hello",
+   "query": "subject=hello"
   },
   "javascript:alert('hello');" : {
    "href": "javascript:alert('hello');",
