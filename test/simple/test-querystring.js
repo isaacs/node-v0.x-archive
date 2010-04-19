55c5485a121a9474184a345012bd26ee32b70145
isaacs <i@izs.me>
2010-04-19 16:27:32 -0700

    Expose the throwing behavior when querystring gets bad utf8 data.

--- test/simple/test-querystring.js
+++ test/simple/test-querystring.js
@@ -14,6 +14,10 @@ var qsTestCases = [
   ["my+weird+field=q1%212%22%27w%245%267%2Fz8%29%3F", "my%20weird%20field=q1!2%22'w%245%267%2Fz8)%3F", {"my weird field" : "q1!2\"'w$5&7/z8)?" }],
   ["foo%3Dbaz=bar", "foo%3Dbaz=bar", {"foo=baz" : "bar"}],
   ["foo=baz=bar", "foo=baz%3Dbar", {"foo" : "baz=bar"}],
+  ["twentyapples=%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF", "twentyapples=%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF%EF%A3%BF", {"twentyapples":""}],
+  ["x=%92%5e%1b%94%98yx%f3%97%ea%db%fa%10d%be%fe%0a5%8c%0a",
+    "x=%EF%BF%BD%5E%1B%EF%BF%BD%EF%BF%BDyx%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%10d%EF%BF%BD%EF%BF%BD%0A5%EF%BF%BD%0A",
+    {"x" : "\ufffd^\u001b\ufffd\ufffdyx\ufffd\ufffd\ufffd\ufffd\ufffd\u0010d\ufffd\ufffd\n5\ufffd\n"}],
     [ "str=foo&arr[]=1&arr[]=2&arr[]=3&obj[a]=bar&obj[b][]=4&obj[b][]=5&obj[b][]=6&obj[b][]=&obj[c][]=4&obj[c][]=5&obj[c][][somestr]=baz&obj[objobj][objobjstr]=blerg&somenull=&undef=", "str=foo&arr%5B%5D=1&arr%5B%5D=2&arr%5B%5D=3&obj%5Ba%5D=bar&obj%5Bb%5D%5B%5D=4&obj%5Bb%5D%5B%5D=5&obj%5Bb%5D%5B%5D=6&obj%5Bb%5D%5B%5D=&obj%5Bc%5D%5B%5D=4&obj%5Bc%5D%5B%5D=5&obj%5Bc%5D%5B%5D%5Bsomestr%5D=baz&obj%5Bobjobj%5D%5Bobjobjstr%5D=blerg&somenull=&undef=", {
     "str":"foo",
     "arr":[1,2,3],
@@ -28,8 +32,7 @@ var qsTestCases = [
   }],
   ["foo[bar][bla]=baz&foo[bar][bla]=blo", "foo%5Bbar%5D%5Bbla%5D%5B%5D=baz&foo%5Bbar%5D%5Bbla%5D%5B%5D=blo", {"foo":{"bar":{"bla":["baz","blo"]}}}],
   ["foo[bar][][bla]=baz&foo[bar][][bla]=blo", "foo%5Bbar%5D%5B%5D%5Bbla%5D=baz&foo%5Bbar%5D%5B%5D%5Bbla%5D=blo", {"foo":{"bar":[{"bla":"baz"},{"bla":"blo"}]}}],
-  ["foo[bar][bla][]=baz&foo[bar][bla][]=blo", "foo%5Bbar%5D%5Bbla%5D%5B%5D=baz&foo%5Bbar%5D%5Bbla%5D%5B%5D=blo", {"foo":{"bar":{"bla":["baz","blo"]}}}],
-  [" foo = bar ", "foo=bar", {"foo":"bar"}]
+  ["foo[bar][bla][]=baz&foo[bar][bla][]=blo", "foo%5Bbar%5D%5Bbla%5D%5B%5D=baz&foo%5Bbar%5D%5Bbla%5D%5B%5D=blo", {"foo":{"bar":{"bla":["baz","blo"]}}}]
 ];
 
 // [ wonkyQS, canonicalQS, obj ]
