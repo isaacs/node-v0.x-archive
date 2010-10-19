aa7cb4dfd840d54cf173f64037ad161252b4e299
isaacs <i@izs.me>
2010-10-18 18:17:33 -0700

    Add require.resolve.  Also, hack the repl so that it works as expected there, too.

--- test/simple/test-require-resolve.js
+++ test/simple/test-require-resolve.js
@@ -3,10 +3,8 @@ var fixturesDir = common.fixturesDir;
 var assert = common.assert;
 var path = require("path");
 
-assert.equal(path.join(__dirname, "../fixtures/a.js"),
-             require.resolve("../fixtures/a"));
-assert.equal(path.join(fixturesDir, "a.js"),
-             require.resolve(path.join(fixturesDir, "a")));
+assert.equal(path.join(__dirname, "../fixtures/a.js"), require.resolve("../fixtures/a"));
+assert.equal(path.join(fixturesDir, "a.js"), require.resolve(path.join(fixturesDir, "a")));
 assert.equal(path.join(fixturesDir, "nested-index", "one", "index.js"),
              require.resolve("../fixtures/nested-index/one"));
 assert.equal("path", require.resolve("path"));
