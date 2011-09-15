8b9ef7ccdf65a4869276e343c70fd2bd3847313c
isaacs <i@izs.me>
2011-09-14 18:04:46 -0700

    Do more in js, use _ on members, no throwing in C++

--- src/node_zlib.cc
+++ src/node_zlib.cc
@@ -46,7 +46,6 @@ using namespace v8;
 typedef ReqWrap<uv_work_t> WorkReqWrap;
 
 static Persistent<String> ondata_sym;
-static Persistent<String> buffer_sym;
 static Persistent<String> callback_sym;
 
 enum node_zlib_mode {
@@ -58,98 +57,65 @@ enum node_zlib_mode {
   INFLATERAW
 };
 
-template <node_zlib_mode mode> class Zlib;
+template <node_zlib_mode mode> class ZCtx;
 
 
 void InitZlib(v8::Handle<v8::Object> target);
-
-const char * zlib_perr(int code)
-{
-  switch (code) {
-    case Z_ERRNO        : return "Z_ERRNO";
-    case Z_STREAM_ERROR : return "Z_STREAM_ERROR";
-    case Z_DATA_ERROR   : return "Z_DATA_ERROR";
-    case Z_MEM_ERROR    : return "Z_MEM_ERROR";
-    case Z_BUF_ERROR    : return "Z_BUF_ERROR";
-    case Z_VERSION_ERROR: return "Z_VERSION_ERROR";
-    default             : return "Unknown Error";
-  }
-}
-
 
 
 
 /**
  * Deflate/Inflate
  */
-template <node_zlib_mode mode> class Zlib : public ObjectWrap {
+template <node_zlib_mode mode> class ZCtx : public ObjectWrap {
 
  public:
 
-  Zlib(int chunk_size,
-        int level,
-        int windowBits,
-        int memLevel,
-        int strategy) : ObjectWrap() {
-    Init(chunk_size, level, windowBits, memLevel, strategy);
+  ZCtx() : ObjectWrap() {
   }
 
-  ~Zlib() {
+  ~ZCtx() {
     if (mode == DEFLATE || mode == GZIP || mode == DEFLATERAW) {
-      (void)deflateEnd(&strm);
+      (void)deflateEnd(&strm_);
     } else if (mode == INFLATE || mode == GUNZIP || mode == INFLATERAW) {
-      (void)inflateEnd(&strm);
+      (void)inflateEnd(&strm_);
     }
-    free(out);
+    free(out_);
   }
 
   // write(chunk, flush, cb)
-  static Handle<Value> Write(const Arguments& args) {
-
-    if (args.Length() != 3) {
-        return ThrowException(Exception::Error(
-                    String::New("usage: write(chunk, flush, cb)")));
+  static Handle<Value>
+  Write(const Arguments& args) {
+    if (args.Length() != 2) {
+      assert(0 && "usage: write(chunk, flush)");
     }
 
     Bytef* buf;
     size_t len;
-    Local<Object> buffer_obj;
-    Local<Function> callback;
 
     if (args[0]->IsNull()) {
       buf = (Bytef *)"\0";
       len = 0;
     } else if (Buffer::HasInstance(args[0])) {
+      Local<Object> buffer_obj;
       buffer_obj = args[0]->ToObject();
       buf = (Bytef *)Buffer::Data(buffer_obj);
       len = Buffer::Length(buffer_obj);
     } else {
-      return ThrowException(Exception::Error(
-                  String::New("First arg must be a buffer or null")));
+      assert(0 && "First arg must be a buffer or null");
     }
 
     unsigned int flush = args[1]->Uint32Value();
 
-    if (!args[2]->IsFunction()) {
-      return ThrowException(Exception::Error(
-                  String::New("Last argument must be a function")));
-    }
-
-    callback = Local<Function>::Cast(args[2]);
-
-    Zlib<mode> *self = ObjectWrap::Unwrap< Zlib<mode> >(args.This());
+    ZCtx<mode> *ctx = ObjectWrap::Unwrap< ZCtx<mode> >(args.This());
+    assert(ctx->init_done_ && "init not done");
 
     WorkReqWrap *req_wrap = new WorkReqWrap();
-    if (!buffer_obj.IsEmpty()) {
-      req_wrap->object_->Set(buffer_sym, buffer_obj);
-    }
-    req_wrap->object_->Set(callback_sym, callback);
-    req_wrap->data_ = self;
 
-    z_stream *strm = &(self->strm);
-    strm->avail_in = len;
-    strm->next_in = buf;
-    self->flush = flush;
+    req_wrap->data_ = ctx;
+    ctx->strm_.avail_in = len;
+    ctx->strm_.next_in = buf;
+    ctx->flush_ = flush;
 
     // build up the work request
     uv_work_t* work_req = new uv_work_t();
@@ -157,8 +123,8 @@ template <node_zlib_mode mode> class Zlib : public ObjectWrap {
 
     uv_queue_work(uv_default_loop(),
                   work_req,
-                  Zlib<mode>::Process,
-                  Zlib<mode>::After);
+                  ZCtx<mode>::Process,
+                  ZCtx<mode>::After);
 
     req_wrap->Dispatched();
 
@@ -170,24 +136,24 @@ template <node_zlib_mode mode> class Zlib : public ObjectWrap {
   // This function may be called multiple times on the uv_work pool
   // for a single write() call, until all of the input bytes have
   // been consumed.
-  static void Process(uv_work_t* work_req) {
+  static void
+  Process(uv_work_t* work_req) {
     WorkReqWrap *req_wrap = (WorkReqWrap *)work_req->data;
-    Zlib<mode> *self = (Zlib<mode> *)req_wrap->data_;
-    z_stream *strm = &(self->strm);
+    ZCtx<mode> *ctx = (ZCtx<mode> *)req_wrap->data_;
 
-    strm->avail_out = self->chunk_size;
-    strm->next_out = self->out;
+    ctx->strm_.avail_out = ctx->chunk_size_;
+    ctx->strm_.next_out = ctx->out_;
 
     // If the avail_out is left at 0, then it means that it ran out
     // of room.  If there was avail_out left over, then it means
     // that all of the input was consumed.
     if (mode == DEFLATE || mode == GZIP || mode == DEFLATERAW) {
-      self->err = deflate(strm, self->flush);
+      ctx->err_ = deflate(&(ctx->strm_), ctx->flush_);
     } else if (mode == INFLATE || mode == GUNZIP || mode == INFLATERAW) {
-      self->err = inflate(strm, self->flush);
+      ctx->err_ = inflate(&(ctx->strm_), ctx->flush_);
     }
 
-    assert(self->err != Z_STREAM_ERROR);
+    assert(ctx->err_ != Z_STREAM_ERROR);
 
     // now After will emit the output, and
     // either schedule another call to Process,
@@ -195,193 +161,158 @@ template <node_zlib_mode mode> class Zlib : public ObjectWrap {
   }
 
   // v8 land!
-  static void After(uv_work_t* work_req) {
+  static void
+  After(uv_work_t* work_req) {
     WorkReqWrap *req_wrap = (WorkReqWrap *)work_req->data;
-    Zlib<mode> *self = (Zlib<mode> *)req_wrap->data_;
-    z_stream *strm = &(self->strm);
+    ZCtx<mode> *ctx = (ZCtx<mode> *)req_wrap->data_;
 
-    int have = self->chunk_size - strm->avail_out;
-
-    // Handle<Value> n = self->handle_
-    //                       ->ToObject()->Get(String::New("prototype"))
-    //                       ->ToObject()->Get(String::New("constructor"))
-    //                       ->ToObject()->Get(String::New("name"));
-    // String::Utf8Value name(n->ToString());
+    int have = ctx->chunk_size_ - ctx->strm_.avail_out;
 
     if (have > 0) {
       // got some output
-      Buffer* flated = Buffer::New((char *)(self->out), have);
-      if (self->handle_->Has(ondata_sym)) {
-        Handle<Value> od = self->handle_->Get(ondata_sym);
+      Buffer* flated = Buffer::New((char *)(ctx->out_), have);
+      if (ctx->handle_->Has(ondata_sym)) {
+        Handle<Value> od = ctx->handle_->Get(ondata_sym);
         assert(od->IsFunction());
         Handle<Function> ondata = Handle<Function>::Cast(od);
         Handle<Value> odargv[1] = { flated->handle_ };
-        ondata->Call(self->handle_, 1, odargv);
+        ondata->Call(ctx->handle_, 1, odargv);
       }
-      // necessary?
-      // memset(self->out, '\0', have);
     }
 
     // if there's no avail_out, then it means that it wasn't able to
     // fully consume the input.  Reschedule another call to Process.
-    if (strm->avail_out == 0) {
+    if (ctx->strm_.avail_out == 0) {
       uv_queue_work(uv_default_loop(),
                     work_req,
-                    Zlib<mode>::Process,
-                    Zlib<mode>::After);
+                    ZCtx<mode>::Process,
+                    ZCtx<mode>::After);
       return;
     }
 
     // call the write() cb
+    assert(req_wrap->object_->Get(callback_sym)->IsFunction() &&
+           "Invalid callback");
     Local<Function> callback =
       Local<Function>::Cast(req_wrap->object_->Get(callback_sym));
-    callback->Call(self->handle_, 0, NULL);
+    callback->Call(ctx->handle_, 0, NULL);
 
     // delete the ReqWrap
     delete req_wrap;
   }
 
-  static Handle<Value> New(const Arguments& args) {
+  static Handle<Value>
+  New(const Arguments& args) {
     HandleScope scope;
-
-    Zlib<mode> *self;
-
-    int chunk_size_ = args[0]->Uint32Value();
-    if (chunk_size_ == 0) {
-      chunk_size_ = DEFAULT_CHUNK;
-    }
-
-    int level_ = args[1]->Int32Value();
-    if (level_ < -1 || level_ > 9) {
-      return ThrowException(Exception::Error(
-            String::New("Invalid compression level, must be -1 to 9")));
-    }
-
-    int windowBits_ = args[2]->Int32Value();
-    if (windowBits_ < 8 || windowBits_ > 15) {
-      return ThrowException(Exception::Error(
-            String::New("Invalid windowBits, must be 8 to 15")));
-    }
-
-    // memLevel and strategy only make sense for compression.
-    int memLevel_ = 8;
-    int strategy_ = Z_DEFAULT_STRATEGY;
-    if (mode == DEFLATE || mode == GZIP || mode == DEFLATERAW) {
-      if (args.Length() > 3) {
-        memLevel_ = args[3]->Int32Value();
-        if (memLevel_ < 1 || memLevel_ > 9) {
-          return ThrowException(Exception::Error(
-                String::New("Invalid memory level, must be 1 to 9")));
-        }
-
-        if (args.Length() > 4) {
-          strategy_ = args[4]->Int32Value();
-          switch (strategy_) {
-            case Z_DEFAULT_STRATEGY:
-            case Z_FILTERED:
-            case Z_HUFFMAN_ONLY:
-            case Z_RLE:
-            case Z_FIXED:
-              break;
-            default:
-              return ThrowException(Exception::Error(
-                    String::New("Invalid compression strategy")));
-          }
-        }
-      }
-    }
-
-
-
-
-    self = new Zlib<mode>(chunk_size_,
-                          level_,
-                          windowBits_,
-                          memLevel_,
-                          strategy_);
-    if (self->err != Z_OK) {
-      const char *msg = self->strm.msg;
-      if (msg == NULL) msg = zlib_perr(self->err);
-      return ThrowException(Exception::Error(String::New(msg)));
-    }
-
-    self->Wrap(args.This());
-
+    ZCtx<mode> *ctx = new ZCtx<mode>();
+    ctx->Wrap(args.This());
     return args.This();
   }
 
+  // just pull the ints out of the args and call the other Init
+  static Handle<Value>
+  Init(const Arguments& args) {
+    HandleScope scope;
 
- private:
+    assert(args.Length() == 5 &&
+           "init(chunkSize, level, windowBits, memLevel, strategy)");
 
-  int err;
-  z_stream strm;
-  int level;
-  int windowBits;
-  int memLevel;
-  int strategy;
+    ZCtx<mode> *ctx = ObjectWrap::Unwrap< ZCtx<mode> >(args.This());
 
-  int flush;
+    int chunk_size = args[0]->Uint32Value();
 
-  Bytef *out;
-  int chunk_size;
+    int windowBits = args[1]->Uint32Value();
+    assert((windowBits >= 8 && windowBits <= 15) && "invalid windowBits");
 
-  void Init (int chunk_size_,
-             int level_,
-             int windowBits_,
-             int memLevel_,
-             int strategy_) {
-    chunk_size = chunk_size_;
-    level = level_;
-    windowBits = windowBits_;
-    memLevel = memLevel_;
-    strategy = strategy_;
+    int level = args[2]->Uint32Value();
+    assert((level >= -1 && level <= 9) && "invalid compression level");
 
-    out = (Bytef *)malloc(chunk_size);
-    // necessary?
-    // memset(out, '\0', chunk_size);
+    int memLevel = args[3]->Uint32Value();
+    assert((memLevel >= 1 && memLevel <= 9) && "invalid memlevel");
 
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
+    int strategy = args[4]->Uint32Value();
+    assert((strategy == Z_FILTERED ||
+            strategy == Z_HUFFMAN_ONLY ||
+            strategy == Z_RLE ||
+            strategy == Z_FIXED ||
+            strategy == Z_DEFAULT_STRATEGY) && "invalid strategy");
 
-    flush = Z_NO_FLUSH;
+    Init(ctx, chunk_size, level, windowBits, memLevel, strategy);
+    return Undefined();
+  }
+
+  static void
+  Init (ZCtx *ctx,
+        int chunk_size,
+        int level,
+        int windowBits,
+        int memLevel,
+        int strategy) {
+    ctx->chunk_size_ = chunk_size;
+    ctx->level_ = level;
+    ctx->windowBits_ = windowBits;
+    ctx->memLevel_ = memLevel;
+    ctx->strategy_ = strategy;
+
+    ctx->out_ = (Bytef *)malloc(ctx->chunk_size_);
+    assert(ctx->out_ && "Couldn't malloc output buffer");
+
+    ctx->strm_.zalloc = Z_NULL;
+    ctx->strm_.zfree = Z_NULL;
+    ctx->strm_.opaque = Z_NULL;
+
+    ctx->flush_ = Z_NO_FLUSH;
 
     if (mode == GZIP || mode == GUNZIP) {
-      windowBits += 16;
+      ctx->windowBits_ += 16;
     }
 
     if (mode == DEFLATERAW || mode == INFLATERAW) {
-      windowBits *= -1;
+      ctx->windowBits_ *= -1;
     }
 
+    int err;
     if (mode == DEFLATE || mode == GZIP || mode == DEFLATERAW) {
-      err = deflateInit2(&strm,
-                         level,
+      err = deflateInit2(&(ctx->strm_),
+                         ctx->level_,
                          Z_DEFLATED,
-                         windowBits,
-                         memLevel,
-                         strategy);
+                         ctx->windowBits_,
+                         ctx->memLevel_,
+                         ctx->strategy_);
     } else if (mode == INFLATE || mode == GUNZIP || mode == INFLATERAW) {
-      err = inflateInit2(&strm, windowBits);
+      err = inflateInit2(&(ctx->strm_), ctx->windowBits_);
     }
 
+    ctx->init_done_ = true;
     assert(err == Z_OK);
   }
+
+ private:
+
+  bool init_done_;
+
+  int err_;
+  z_stream strm_;
+  int level_;
+  int windowBits_;
+  int memLevel_;
+  int strategy_;
+
+  int flush_;
+
+  Bytef *out_;
+  int chunk_size_;
 };
 
 
 #define NODE_ZLIB_CLASS(mode, name)   \
   { \
-    Local<FunctionTemplate> z = FunctionTemplate::New(Zlib<mode>::New); \
+    Local<FunctionTemplate> z = FunctionTemplate::New(ZCtx<mode>::New); \
     z->InstanceTemplate()->SetInternalFieldCount(1); \
-    NODE_SET_PROTOTYPE_METHOD(z, "write", Zlib<mode>::Write); \
+    NODE_SET_PROTOTYPE_METHOD(z, "write", ZCtx<mode>::Write); \
+    NODE_SET_PROTOTYPE_METHOD(z, "init", ZCtx<mode>::Init); \
     z->SetClassName(String::NewSymbol(name)); \
     target->Set(String::NewSymbol(name), z->GetFunction()); \
-  }
-
-#define NODE_ZLIB_CONST(name) \
-  { \
-    target->Set(String::NewSymbol(#name), Integer::New(name)); \
   }
 
 void InitZlib(Handle<Object> target) {
@@ -395,34 +326,33 @@ void InitZlib(Handle<Object> target) {
   NODE_ZLIB_CLASS(GUNZIP, "Gunzip")
 
   ondata_sym = NODE_PSYMBOL("onData");
-  buffer_sym = NODE_PSYMBOL("buffer");
   callback_sym = NODE_PSYMBOL("callback");
 
   NODE_DEFINE_CONSTANT(target, Z_NO_FLUSH);
   NODE_DEFINE_CONSTANT(target, Z_PARTIAL_FLUSH);
-  NODE_DEFINE_CONSTANT(target, Z_SYNC_FLUSH    );
-  NODE_DEFINE_CONSTANT(target, Z_FULL_FLUSH    );
-  NODE_DEFINE_CONSTANT(target, Z_FINISH        );
-  NODE_DEFINE_CONSTANT(target, Z_BLOCK         );
-  NODE_DEFINE_CONSTANT(target, Z_OK            );
-  NODE_DEFINE_CONSTANT(target, Z_STREAM_END    );
-  NODE_DEFINE_CONSTANT(target, Z_NEED_DICT     );
-  NODE_DEFINE_CONSTANT(target, Z_ERRNO        );
-  NODE_DEFINE_CONSTANT(target, Z_STREAM_ERROR );
-  NODE_DEFINE_CONSTANT(target, Z_DATA_ERROR   );
-  NODE_DEFINE_CONSTANT(target, Z_MEM_ERROR    );
-  NODE_DEFINE_CONSTANT(target, Z_BUF_ERROR    );
+  NODE_DEFINE_CONSTANT(target, Z_SYNC_FLUSH);
+  NODE_DEFINE_CONSTANT(target, Z_FULL_FLUSH);
+  NODE_DEFINE_CONSTANT(target, Z_FINISH);
+  NODE_DEFINE_CONSTANT(target, Z_BLOCK);
+  NODE_DEFINE_CONSTANT(target, Z_OK);
+  NODE_DEFINE_CONSTANT(target, Z_STREAM_END);
+  NODE_DEFINE_CONSTANT(target, Z_NEED_DICT);
+  NODE_DEFINE_CONSTANT(target, Z_ERRNO);
+  NODE_DEFINE_CONSTANT(target, Z_STREAM_ERROR);
+  NODE_DEFINE_CONSTANT(target, Z_DATA_ERROR);
+  NODE_DEFINE_CONSTANT(target, Z_MEM_ERROR);
+  NODE_DEFINE_CONSTANT(target, Z_BUF_ERROR);
   NODE_DEFINE_CONSTANT(target, Z_VERSION_ERROR);
-  NODE_DEFINE_CONSTANT(target, Z_NO_COMPRESSION       );
-  NODE_DEFINE_CONSTANT(target, Z_BEST_SPEED           );
-  NODE_DEFINE_CONSTANT(target, Z_BEST_COMPRESSION     );
-  NODE_DEFINE_CONSTANT(target, Z_DEFAULT_COMPRESSION  );
-  NODE_DEFINE_CONSTANT(target, Z_FILTERED        );
-  NODE_DEFINE_CONSTANT(target, Z_HUFFMAN_ONLY    );
-  NODE_DEFINE_CONSTANT(target, Z_RLE             );
-  NODE_DEFINE_CONSTANT(target, Z_FIXED           );
+  NODE_DEFINE_CONSTANT(target, Z_NO_COMPRESSION);
+  NODE_DEFINE_CONSTANT(target, Z_BEST_SPEED);
+  NODE_DEFINE_CONSTANT(target, Z_BEST_COMPRESSION);
+  NODE_DEFINE_CONSTANT(target, Z_DEFAULT_COMPRESSION);
+  NODE_DEFINE_CONSTANT(target, Z_FILTERED);
+  NODE_DEFINE_CONSTANT(target, Z_HUFFMAN_ONLY);
+  NODE_DEFINE_CONSTANT(target, Z_RLE);
+  NODE_DEFINE_CONSTANT(target, Z_FIXED);
   NODE_DEFINE_CONSTANT(target, Z_DEFAULT_STRATEGY);
-  NODE_DEFINE_CONSTANT(target, ZLIB_VERNUM );
+  NODE_DEFINE_CONSTANT(target, ZLIB_VERNUM);
 
   target->Set(String::NewSymbol("ZLIB_VERSION"), String::New(ZLIB_VERSION));
 }
