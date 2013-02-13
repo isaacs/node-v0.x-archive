74e8bdef2a8f8327847bca0200904924795edf4c
isaacs <i@izs.me>
2013-02-13 12:20:36 -0800

    bench: Use wrk for http benchmarking

--- benchmark/common.js
+++ benchmark/common.js
@@ -11,7 +11,6 @@ if (module === require.main) {
     process.exit(1);
   }
 
-  var path = require('path');
   var fs = require('fs');
   var dir = path.join(__dirname, type);
   var tests = fs.readdirSync(dir);
@@ -59,16 +58,16 @@ function Benchmark(fn, options) {
   });
 }
 
-// run ab against a server.
-Benchmark.prototype.ab = function(path, args, cb) {
-  var url = 'http://127.0.0.1:' + exports.PORT + path;
+// benchmark an http server.
+Benchmark.prototype.http = function(p, args, cb) {
+  var url = 'http://127.0.0.1:' + exports.PORT + p;
   args.push(url);
 
   var self = this;
   var out = '';
   var spawn = require('child_process').spawn;
-  // console.error('ab %s', args.join(' '));
-  var child = spawn('ab', args);
+  var wrk = path.resolve(__dirname, '..', 'tools', 'wrk', 'wrk');
+  var child = spawn(wrk, args);
 
   child.stdout.setEncoding('utf8');
 
@@ -81,14 +80,14 @@ Benchmark.prototype.ab = function(path, args, cb) {
       cb(code);
 
     if (code) {
-      console.error('ab failed with ' + code);
+      console.error('wrk failed with ' + code);
       process.exit(code)
     }
-    var m = out.match(/Requests per second: +([0-9\.]+)/);
+    var m = out.match(/Requests\/sec:[ \t]+([0-9\.]+)/);
     var qps = m && +m[1];
     if (!qps) {
-      process.stderr.write(out + '\n');
-      console.error('ab produced strange output');
+      console.error('%j', out);
+      console.error('wrk produced strange output');
       process.exit(1);
     }
     self.report(+qps);
