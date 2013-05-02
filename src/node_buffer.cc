f630be0d3a8fddecfff49719c597d9d2675d0068
isaacs <i@izs.me>
2013-05-02 10:44:45 -0700

    buffer: DRY string decoding using StringBytes

--- src/node_buffer.cc
+++ src/node_buffer.cc
@@ -20,8 +20,10 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-#include "node.h"
 #include "node_buffer.h"
+
+#include "node.h"
+#include "string_bytes.h"
 
 #include "v8.h"
 #include "v8-profiler.h"
@@ -64,49 +66,6 @@ static Persistent<String> chars_written_sym;
 static Persistent<String> write_sym;
 static Persistent<Function> fast_buffer_constructor;
 Persistent<FunctionTemplate> Buffer::constructor_template;
-
-
-static inline size_t base64_decoded_size(const char *src, size_t size) {
-  const char *const end = src + size;
-  const int remainder = size % 4;
-
-  size = (size / 4) * 3;
-  if (remainder) {
-    if (size == 0 && remainder == 1) {
-      // special case: 1-byte input cannot be decoded
-      size = 0;
-    } else {
-      // non-padded input, add 1 or 2 extra bytes
-      size += 1 + (remainder == 3);
-    }
-  }
-
-  // check for trailing padding (1 or 2 bytes)
-  if (size > 0) {
-    if (end[-1] == '=') size--;
-    if (end[-2] == '=') size--;
-  }
-
-  return size;
-}
-
-
-static size_t ByteLength (Handle<String> string, enum encoding enc) {
-  HandleScope scope;
-
-  if (enc == UTF8) {
-    return string->Utf8Length();
-  } else if (enc == BASE64) {
-    String::Utf8Value v(string);
-    return base64_decoded_size(*v, v.length());
-  } else if (enc == UCS2) {
-    return string->Length() * 2;
-  } else if (enc == HEX) {
-    return string->Length() / 2;
-  } else {
-    return string->Length();
-  }
-}
 
 
 Handle<Object> Buffer::New(Handle<String> string) {
@@ -242,7 +201,7 @@ Handle<Value> Buffer::BinarySlice(const Arguments &args) {
   char *data = parent->data_ + start;
   //Local<String> string = String::New(data, end - start);
 
-  Local<Value> b =  Encode(data, end - start, BINARY);
+  Local<Value> b = Encode(data, end - start, BINARY);
 
   return scope.Close(b);
 }
@@ -410,28 +369,6 @@ Handle<Value> Buffer::HexSlice(const Arguments &args) {
 }
 
 
-// supports regular and URL-safe base64
-static const int unbase64_table[] =
-  {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-2,-1,-1,-2,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-2,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,62,-1,62,-1,63
-  ,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-1,-1,-1
-  ,-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14
-  ,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,63
-  ,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40
-  ,41,42,43,44,45,46,47,48,49,50,51,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  ,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-  };
-#define unbase64(x) unbase64_table[(uint8_t)(x)]
-
-
 Handle<Value> Buffer::Base64Slice(const Arguments &args) {
   HandleScope scope;
   Buffer *parent = ObjectWrap::Unwrap<Buffer>(args.This());
@@ -573,287 +510,77 @@ Handle<Value> Buffer::Copy(const Arguments &args) {
 }
 
 
-// var charsWritten = buffer.utf8Write(string, offset, [maxLength]);
+Handle<Value> Buffer::Base64Write(const Arguments &args) {
+  return Buffer::StringWrite<BASE64>(args);
+}
+
+Handle<Value> Buffer::BinaryWrite(const Arguments &args) {
+  return Buffer::StringWrite<BINARY>(args);
+}
+
 Handle<Value> Buffer::Utf8Write(const Arguments &args) {
+  return Buffer::StringWrite<UTF8>(args);
+}
+
+Handle<Value> Buffer::Ucs2Write(const Arguments &args) {
+  return Buffer::StringWrite<UCS2>(args);
+}
+
+Handle<Value> Buffer::HexWrite(const Arguments& args) {
+  return Buffer::StringWrite<HEX>(args);
+}
+
+Handle<Value> Buffer::AsciiWrite(const Arguments &args) {
+  return Buffer::StringWrite<ASCII>(args);
+}
+
+template <encoding encoding>
+Handle<Value> Buffer::StringWrite(const Arguments &args) {
   HandleScope scope;
-  Buffer *buffer = ObjectWrap::Unwrap<Buffer>(args.This());
+
+  Buffer* buffer = ObjectWrap::Unwrap<Buffer>(args.This());
 
   if (!args[0]->IsString()) {
-    return ThrowException(Exception::TypeError(String::New(
-            "Argument must be a string")));
+    return ThrowTypeError("Argument must be a string");
   }
 
-  Local<String> s = args[0]->ToString();
+  Local<String> str = args[0].As<String>();
 
-  size_t offset = args[1]->Uint32Value();
+  if (encoding == HEX && str->Length() % 2 != 0)
+    return ThrowTypeError("Invalid hex string");
 
-  int length = s->Length();
-
-  if (length == 0) {
-    constructor_template->GetFunction()->Set(chars_written_sym,
-                                             Integer::New(0));
-    return scope.Close(Integer::New(0));
-  }
-
-  if (length > 0 && offset >= buffer->length_) {
-    return ThrowTypeError("Offset is out of bounds");
-  }
-
+  size_t offset = args[1]->Int32Value();
   size_t max_length = args[2]->IsUndefined() ? buffer->length_ - offset
                                              : args[2]->Uint32Value();
   max_length = MIN(buffer->length_ - offset, max_length);
 
-  char* p = buffer->data_ + offset;
+  if (max_length == 0) {
+    // shortcut: nothing to write anyway
+    Local<Integer> val = Integer::New(0);
+    constructor_template->GetFunction()->Set(chars_written_sym, val);
+    return scope.Close(val);
+  }
 
+  if (encoding == UCS2)
+    max_length = max_length / 2;
+
+  if (offset >= buffer->length_) {
+    return ThrowTypeError("Offset is out of bounds");
+  }
+
+  char* start = buffer->data_ + offset;
   int char_written;
-
-  int written = s->WriteUtf8(p,
-                             max_length,
-                             &char_written,
-                             (String::HINT_MANY_WRITES_EXPECTED |
-                              String::NO_NULL_TERMINATION));
+  size_t written = StringBytes::Write(start,
+                                      max_length,
+                                      str,
+                                      encoding,
+                                      &char_written);
 
   constructor_template->GetFunction()->Set(chars_written_sym,
                                            Integer::New(char_written));
 
   return scope.Close(Integer::New(written));
 }
-
-
-// var charsWritten = buffer.ucs2Write(string, offset, [maxLength]);
-Handle<Value> Buffer::Ucs2Write(const Arguments &args) {
-  HandleScope scope;
-  Buffer *buffer = ObjectWrap::Unwrap<Buffer>(args.This());
-
-  if (!args[0]->IsString()) {
-    return ThrowTypeError("Argument must be a string");
-  }
-
-  Local<String> s = args[0]->ToString();
-
-  size_t offset = args[1]->Uint32Value();
-
-  if (s->Length() > 0 && offset >= buffer->length_) {
-    return ThrowException(Exception::TypeError(String::New(
-            "Offset is out of bounds")));
-  }
-
-  size_t max_length = args[2]->IsUndefined() ? buffer->length_ - offset
-                                             : args[2]->Uint32Value();
-  max_length = MIN(buffer->length_ - offset, max_length) / 2;
-
-  uint16_t* p = (uint16_t*)(buffer->data_ + offset);
-
-  int written = s->Write(p,
-                         0,
-                         max_length,
-                         (String::HINT_MANY_WRITES_EXPECTED |
-                          String::NO_NULL_TERMINATION));
-
-  constructor_template->GetFunction()->Set(chars_written_sym,
-                                           Integer::New(written));
-
-  return scope.Close(Integer::New(written * 2));
-}
-
-
-inline unsigned hex2bin(char c) {
-  if (c >= '0' && c <= '9') return c - '0';
-  if (c >= 'A' && c <= 'F') return 10 + (c - 'A');
-  if (c >= 'a' && c <= 'f') return 10 + (c - 'a');
-  return static_cast<unsigned>(-1);
-}
-
-
-Handle<Value> Buffer::HexWrite(const Arguments& args) {
-  HandleScope scope;
-  Buffer* parent = ObjectWrap::Unwrap<Buffer>(args.This());
-
-  if (args[0]->IsString() == false) {
-    return ThrowTypeError("Argument must be a string");
-  }
-
-  Local<String> s = args[0].As<String>();
-
-  if (s->Length() % 2 != 0) {
-    return ThrowTypeError("Invalid hex string");
-  }
-
-  uint32_t start = args[1]->Uint32Value();
-  uint32_t size = args[2]->Uint32Value();
-  uint32_t end = start + size;
-
-  if (start >= parent->length_) {
-    Local<Integer> val = Integer::New(0);
-    constructor_template->GetFunction()->Set(chars_written_sym, val);
-    return scope.Close(val);
-  }
-
-  if (end < start || end > parent->length_) {  // Overflow + bounds check.
-    end = parent->length_;
-    size = parent->length_ - start;
-  }
-
-  if (size == 0) {
-    Local<Integer> val = Integer::New(0);
-    constructor_template->GetFunction()->Set(chars_written_sym, val);
-    return scope.Close(val);
-  }
-
-  char* dst = parent->data_ + start;
-  String::AsciiValue string(s);
-  const char* src = *string;
-  uint32_t max = string.length() / 2;
-
-  if (max > size) {
-    max = size;
-  }
-
-  for (uint32_t i = 0; i < max; ++i) {
-    unsigned a = hex2bin(src[i * 2 + 0]);
-    unsigned b = hex2bin(src[i * 2 + 1]);
-    if (!~a || !~b) return ThrowTypeError("Invalid hex string");
-    dst[i] = a * 16 + b;
-  }
-
-  constructor_template->GetFunction()->Set(chars_written_sym,
-                                           Integer::New(max * 2));
-
-  return scope.Close(Integer::New(max));
-}
-
-
-// var charsWritten = buffer.asciiWrite(string, offset);
-Handle<Value> Buffer::AsciiWrite(const Arguments &args) {
-  HandleScope scope;
-
-  Buffer *buffer = ObjectWrap::Unwrap<Buffer>(args.This());
-
-  if (!args[0]->IsString()) {
-    return ThrowTypeError("Argument must be a string");
-  }
-
-  Local<String> s = args[0]->ToString();
-  size_t length = s->Length();
-  size_t offset = args[1]->Int32Value();
-
-  if (length > 0 && offset >= buffer->length_) {
-    return ThrowTypeError("Offset is out of bounds");
-  }
-
-  size_t max_length = args[2]->IsUndefined() ? buffer->length_ - offset
-                                             : args[2]->Uint32Value();
-  max_length = MIN(length, MIN(buffer->length_ - offset, max_length));
-
-  char *p = buffer->data_ + offset;
-
-  int written = s->WriteAscii(p,
-                              0,
-                              max_length,
-                              (String::HINT_MANY_WRITES_EXPECTED |
-                               String::NO_NULL_TERMINATION));
-
-  constructor_template->GetFunction()->Set(chars_written_sym,
-                                           Integer::New(written));
-
-  return scope.Close(Integer::New(written));
-}
-
-
-// var bytesWritten = buffer.base64Write(string, offset, [maxLength]);
-Handle<Value> Buffer::Base64Write(const Arguments &args) {
-  HandleScope scope;
-
-  Buffer *buffer = ObjectWrap::Unwrap<Buffer>(args.This());
-
-  if (!args[0]->IsString()) {
-    return ThrowTypeError("Argument must be a string");
-  }
-
-  String::AsciiValue s(args[0]);
-  size_t length = s.length();
-  size_t offset = args[1]->Int32Value();
-  size_t max_length = args[2]->IsUndefined() ? buffer->length_ - offset
-                                             : args[2]->Uint32Value();
-  max_length = MIN(length, MIN(buffer->length_ - offset, max_length));
-
-  if (max_length && offset >= buffer->length_) {
-    return ThrowTypeError("Offset is out of bounds");
-  }
-
-  char a, b, c, d;
-  char* start = buffer->data_ + offset;
-  char* dst = start;
-  char* const dstEnd = dst + max_length;
-  const char* src = *s;
-  const char* const srcEnd = src + s.length();
-
-  while (src < srcEnd && dst < dstEnd) {
-    int remaining = srcEnd - src;
-
-    while (unbase64(*src) < 0 && src < srcEnd) src++, remaining--;
-    if (remaining == 0 || *src == '=') break;
-    a = unbase64(*src++);
-
-    while (unbase64(*src) < 0 && src < srcEnd) src++, remaining--;
-    if (remaining <= 1 || *src == '=') break;
-    b = unbase64(*src++);
-
-    *dst++ = (a << 2) | ((b & 0x30) >> 4);
-    if (dst == dstEnd) break;
-
-    while (unbase64(*src) < 0 && src < srcEnd) src++, remaining--;
-    if (remaining <= 2 || *src == '=') break;
-    c = unbase64(*src++);
-
-    *dst++ = ((b & 0x0F) << 4) | ((c & 0x3C) >> 2);
-    if (dst == dstEnd) break;
-
-    while (unbase64(*src) < 0 && src < srcEnd) src++, remaining--;
-    if (remaining <= 3 || *src == '=') break;
-    d = unbase64(*src++);
-
-    *dst++ = ((c & 0x03) << 6) | (d & 0x3F);
-  }
-
-  constructor_template->GetFunction()->Set(chars_written_sym,
-                                           Integer::New(dst - start));
-
-  return scope.Close(Integer::New(dst - start));
-}
-
-
-Handle<Value> Buffer::BinaryWrite(const Arguments &args) {
-  HandleScope scope;
-
-  Buffer *buffer = ObjectWrap::Unwrap<Buffer>(args.This());
-
-  if (!args[0]->IsString()) {
-    return ThrowTypeError("Argument must be a string");
-  }
-
-  Local<String> s = args[0]->ToString();
-  size_t length = s->Length();
-  size_t offset = args[1]->Int32Value();
-
-  if (s->Length() > 0 && offset >= buffer->length_) {
-    return ThrowTypeError("Offset is out of bounds");
-  }
-
-  char *p = (char*)buffer->data_ + offset;
-
-  size_t max_length = args[2]->IsUndefined() ? buffer->length_ - offset
-                                             : args[2]->Uint32Value();
-  max_length = MIN(length, MIN(buffer->length_ - offset, max_length));
-
-  int written = DecodeWrite(p, max_length, s, BINARY);
-
-  constructor_template->GetFunction()->Set(chars_written_sym,
-                                           Integer::New(written));
-
-  return scope.Close(Integer::New(written));
-}
-
 
 static bool is_big_endian() {
   const union { uint8_t u8[2]; uint16_t u16; } u = {{0, 1}};
@@ -983,7 +710,7 @@ Handle<Value> Buffer::ByteLength(const Arguments &args) {
   Local<String> s = args[0]->ToString();
   enum encoding e = ParseEncoding(args[1], UTF8);
 
-  return scope.Close(Integer::New(node::ByteLength(s, e)));
+  return scope.Close(Integer::New(StringBytes::SizePrecise(s, e)));
 }
 
 
@@ -1101,17 +828,6 @@ RetainedObjectInfo* WrapperInfo(uint16_t class_id, Handle<Value> wrapper) {
 
 void Buffer::Initialize(Handle<Object> target) {
   HandleScope scope;
-
-  // sanity checks
-  assert(unbase64('/') == 63);
-  assert(unbase64('+') == 62);
-  assert(unbase64('T') == 19);
-  assert(unbase64('Z') == 25);
-  assert(unbase64('t') == 45);
-  assert(unbase64('z') == 51);
-  assert(unbase64(' ') == -2);
-  assert(unbase64('\n') == -2);
-  assert(unbase64('\r') == -2);
 
   length_symbol = NODE_PSYMBOL("length");
   chars_written_sym = NODE_PSYMBOL("_charsWritten");
