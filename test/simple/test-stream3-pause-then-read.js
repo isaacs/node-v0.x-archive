fe96fc8cf391daa723104f92e7c4b95d5ffebb0d
isaacs <i@izs.me>
2013-07-18 12:07:31 -0700

    test make streams3 test better

--- test/simple/test-stream3-pause-then-read.js
+++ test/simple/test-stream3-pause-then-read.js
@@ -26,8 +26,13 @@ var stream = require('stream');
 var Readable = stream.Readable;
 var Writable = stream.Writable;
 
+var totalChunks = 100;
+var chunkSize = 99;
+var expectTotalData = totalChunks * chunkSize;
+var expectEndingData = expectTotalData;
+
 var r = new Readable({ highWaterMark: 1000 });
-var chunks = 100;
+var chunks = totalChunks;
 r._read = function(n) {
   if (!(chunks % 2))
     setImmediate(push);
@@ -39,7 +44,7 @@ r._read = function(n) {
 
 var totalPushed = 0;
 function push() {
-  var chunk = chunks-- > 0 ? new Buffer(99) : null;
+  var chunk = chunks-- > 0 ? new Buffer(chunkSize) : null;
   if (chunk) {
     totalPushed += chunk.length;
     chunk.fill('x');
@@ -51,19 +56,27 @@ read100();
 
 // first we read 100 bytes
 function read100() {
-  console.error('read 100');
-  var c = r.read(100);
-  if (!c)
-    r.once('readable', read100);
-  else {
-    assert.equal(c.length, 100);
-    assert(!r._readableState.flowing);
-    onData();
-  }
+  readn(100, onData);
+}
+
+function readn(n, then) {
+  console.error('read %d', n);
+  expectEndingData -= n;
+  ;(function read() {
+    var c = r.read(n);
+    if (!c)
+      r.once('readable', read);
+    else {
+      assert.equal(c.length, n);
+      assert(!r._readableState.flowing);
+      then();
+    }
+  })();
 }
 
 // then we listen to some data events
 function onData() {
+  expectEndingData -= 100;
   console.error('onData');
   var seen = 0;
   r.on('data', function od(c) {
@@ -88,12 +101,13 @@ function onData() {
 
 // Just pipe 200 bytes, then unshift the extra and unpipe
 function pipeLittle() {
+  expectEndingData -= 200;
   console.error('pipe a little');
   var w = new Writable();
   var written = 0;
   w.on('finish', function() {
     assert.equal(written, 200);
-    setImmediate(pipe);
+    setImmediate(read1234);
   });
   w._write = function(chunk, encoding, cb) {
     written += chunk.length;
@@ -113,6 +127,28 @@ function pipeLittle() {
   r.pipe(w);
 }
 
+// now read 1234 more bytes
+function read1234() {
+  readn(1234, resumePause);
+}
+
+function resumePause() {
+  console.error('resumePause');
+  // don't read anything, just resume and re-pause a whole bunch
+  r.resume();
+  r.pause();
+  r.resume();
+  r.pause();
+  r.resume();
+  r.pause();
+  r.resume();
+  r.pause();
+  r.resume();
+  r.pause();
+  setImmediate(pipe);
+}
+
+
 function pipe() {
   console.error('pipe the rest');
   var w = new Writable();
@@ -123,7 +159,8 @@ function pipe() {
   };
   w.on('finish', function() {
     console.error('written', written, totalPushed);
-    assert.equal(written, totalPushed - 400);
+    assert.equal(written, expectEndingData);
+    assert.equal(totalPushed, expectTotalData);
     console.log('ok');
   });
   r.pipe(w);
