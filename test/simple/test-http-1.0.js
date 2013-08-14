482eb9bbdd1a335c35965dd7b3568469b59fc868
isaacs <i@izs.me>
2013-08-14 09:25:48 -0700

    test fix

--- test/simple/test-http-1.0.js
+++ test/simple/test-http-1.0.js
@@ -106,8 +106,8 @@ function test(handler, request_generator, response_validator) {
     assert.equal(0, req.httpVersionMinor);
     res.sendDate = false;
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello, '); res._send('');
-    res.write('world!'); res._send('');
+    res.write('Hello, ');
+    res.write('world!');
     res.end();
   }
 
@@ -143,8 +143,8 @@ function test(handler, request_generator, response_validator) {
     assert.equal(1, req.httpVersionMinor);
     res.sendDate = false;
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello, '); res._send('');
-    res.write('world!'); res._send('');
+    res.write('Hello, ');
+    res.write('world!');
     res.end();
   }
 
